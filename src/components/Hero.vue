56324faa5fc5b30d3144d8ec8e18587ade4220d3
francesco <francescosanti91@gmail.com>
2021-11-23 17:06:25 +0100

    nuovo componente jumbo

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="jumbotron">
+    <h3>Questo è il jumbotron</h3>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    #jumbotron {
+        height: 400px;
+        width: 80%;
+        margin: 0 auto;
+        background-color: lightgreen;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    h3 {
+        color: blue;
+        font-size: 30px;
+    }
+</style>
